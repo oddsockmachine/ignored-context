362eb1298ee55168fc8677a7a0a0ab6af38e584d
Dave Walker <oddsockmachine@gmail.com>
2014-08-30 22:57:59 +0100

    Added example

--- ignored.py
+++ ignored.py
@@ -4,3 +4,14 @@ def ignored(*exceptions):
         yield
     except exceptions:
         pass
+
+
+# example:
+# with ignored(OSError):
+#     os.remove("temp_file.tmp")
+
+# instead of:
+# try:
+#     os.remove("temp_file.tmp")
+# except OSError:
+#     pass
